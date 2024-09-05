2de802bb9b9271382274303aaed98968d969b432
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-09-05 23:39:57 +0100

    Start

--- src/main/java/Backend/libaryproject/PaymentServiceApplication.java
+++ src/main/java/Backend/libaryproject/PaymentServiceApplication.java
@@ -2,6 +2,7 @@ package Backend.libaryproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
